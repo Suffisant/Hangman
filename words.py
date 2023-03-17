dfbd5a02a67e4dce5aa44817a3f0603baeabf6fe
Suffisant <Suffi3madhikarmi@gmail.com>
2023-03-17 17:10:12 +0545

    Hangman.py

---
+++ words.py
@@ -0,0 +1,1 @@
+words = ["Apple", "Banana", "Carrot", "Dragon", "Elephant", "Flamingo", "Guitar", "Helicopter", "Ice cream", "Jellyfish", "Kangaroo", "Lemon", "Mountain", "Notebook", "Octopus", "Pineapple", "Question", "Raspberry", "Strawberry", "Umbrella", "Volleyball", "Watermelon", "Xylophone", "Yellow", "Zebra"]
